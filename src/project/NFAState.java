e33a3b5e29b0984e1c9fc8ef654a08cb069c7048
Azzeddine NACER <mr.azzeddinenacer@gmail.com>
2020-11-16 20:57:03 +0100

    Update NFAState.java
    Describe data structure

--- src/project/NFAState.java
+++ src/project/NFAState.java
@@ -5,6 +5,10 @@ public class NFAState {
     private boolean isEnd;
     private int name;
     private HashMap<Character, HashSet<NFAState>> children = new HashMap<Character, HashSet<NFAState>>();
+    
+    // NFAState stands for Nondeterministic Finite Automaton State
+    // children : used to map a hashSet of states into a character (ex: using 'a' we go to state 1, 2 and 3); because it's a NFA
+    // We used hashSet instead of array to avoid the process of verifying duplicats.
 
     public NFAState() {
     }
